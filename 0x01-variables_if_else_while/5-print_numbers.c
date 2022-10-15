f4572ff378d1fdfc75cc2d92bdceb48e2c70e790
Aaron Kebede <aarongkebede@gmail.com>
2022-10-14 19:37:35 -0700

    print numbs 0-10 using putchar

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints all single digit numbers of base 10 starting from 0
+ * main - prints single digits of base 10 starting from 0
  * Return: 0
  */
 int main(void)
